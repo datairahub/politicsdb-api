8274201346ac5ec302a16643892eec6112924a78
Santi Espinosa <saigesp@gmail.com>
2023-02-04 12:18:27 +0100

    feat(persons): add Person model

---
+++ backend/persons/models/PersonModel.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+from persons.services.persons_id import person_id_from_name
+from core.models import BaseAbstracModel
+
+
+class Person(BaseAbstracModel):
+    """
+    Persona (física)
+    """
+
+    GENRES = (
+        ("M", "Male"),
+        ("F", "Female"),
+    )
+    full_name = models.CharField(max_length=250, db_index=True)
+    id_name = models.CharField(max_length=250, db_index=True, unique=True)
+    birth_date = models.DateField(null=True, blank=True)
+    # birth_date_source = models.ForeignKey('governors.BirthDateSource', null=True, blank=True, on_delete=models.PROTECT)
+    # birth_date_source_url = models.TextField(null=True, blank=True)
+    # birth_date_exact = models.BooleanField(default=False)
+    genre = models.CharField(choices=GENRES, max_length=1, default=GENRES[0])
+
+    def save(self, *args, **kwargs):
+        self.id_name = person_id_from_name(self.full_name)
+        super(Person, self).save(*args, **kwargs)
+
+    # def get_person_last_name(self):
+    #     if self.metadata.get('www.congreso.es', None):
+    #         last_name = None
+    #         for legislature_key in self.metadata.get('www.congreso.es'):
+    #             last_name = self.metadata['www.congreso.es'][legislature_key].get('apellidos', None)
+    #             if last_name:
+    #                 return last_name
+
+    #     elif self.metadata.get('www.senado.es'):
+    #         return self.metadata['www.senado.es'].get('apellidos', None)
+
+    #     elif self.metadata.get('manual_file'):
+    #         return self.metadata['manual_file'].get('last_name', None)
+
+    #     return None
+
+    def __str__(self):
+        return self.full_name
---
+++ backend/persons/services/persons_id.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import re
+from unidecode import unidecode
+
+
+def remove_double_whitespaces(text: str) -> str:
+    """
+    Remove double whitespaces from strings
+
+    :param text: input text
+    :return: clean text
+    """
+    while "  " in text:
+        text = text.replace("  ", " ")
+    return text.strip()
+
+
+def person_id_from_name(name: str) -> str:
+    """
+    Transform person full name to snakecase to use as ID
+
+    :param name: person full name
+    :return: snake_case name
+    """
+    rm_pattern = re.compile(r"\b\w{1,2}\b")
+    hp_pattern = re.compile(r"[\W_]+")
+    name = remove_double_whitespaces(name.lower())
+    name = unidecode(name)
+    name = rm_pattern.sub("", name)
+    name = remove_double_whitespaces(name)
+    name = hp_pattern.sub("_", name.strip())
+    return name.strip()
